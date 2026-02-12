8efab7f72b7f71860ce3aabf6f6ac54dfd291136
Fellipe Gomes <52552307+Fellipe0408@users.noreply.github.com>
2026-02-12 13:13:38 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const switcher = document.querySelector('.btn');
+
+document.addEventListener('DOMContentLoaded', function() {
+    const switcher = document.querySelector('.btn');
+
+    switcher.addEventListener('click', function() {
+    document.body.classList.toggle('dark-theme');
+
+    if (document.body.classList.contains('dark-theme')) {
+        this.textContent = "Light";
+    } else {
+        this.textContent = "Dark";
+    }
+});
+
+});
